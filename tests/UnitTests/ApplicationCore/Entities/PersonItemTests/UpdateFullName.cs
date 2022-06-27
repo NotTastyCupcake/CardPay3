f63de5d01351cc52625c4cae7744ab069c237e27
NotTastyCupcake <andrei28122000@gmail.com>
2022-06-27 10:17:25 +0500

    tests: Тесты изменения ФИО,Латинские Имя Фамилия для карты сотрудника

---
+++ tests/UnitTests/ApplicationCore/Entities/PersonItemTests/UpdateFullName.cs
@@ -0,0 +1,52 @@
+﻿using Metcom.CardPay3.ApplicationCore.Entities.PersonAggregate;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metcom.CardPay3.UnitTests.ApplicationCore.Entities.PersonItemTests
+{
+    public class UpdateFullName
+    {
+        private PersonItem _testItem;
+
+        private int _validId = 1;
+        private string _validLastName = "TestLastName";
+        private string _validFirstName = "TestFirstName";
+        private string _validMiddleName = "TestMiddleName";
+        private string _validLatinFirstName = "TestFirstLatinName";
+        private string _validLatinLastName = "TestLastLatinName";
+        private int _validGenderId = 1;
+        private int _validDocumentId = 1;
+
+        [SetUp]
+        public void UpdateFullNameSetUp()
+        {
+            _testItem = new PersonItem(_validId,_validLastName,_validFirstName, _validMiddleName, _validLatinFirstName, _validLatinLastName, _validGenderId, _validDocumentId);
+        }
+
+        [TestCase("", "", "")]
+        [TestCase("Test", "", "")]
+        [TestCase("Test", "Test", "")]
+        [TestCase("", "Test", "Test")]
+        [TestCase("", "", "Test")]
+        [TestCase("Test", "", "Test")]
+        public void ThrowsArgumentExceptionGivenEmptyName(string lastName, string firstName, string middleName)
+        {
+            Assert.Throws<ArgumentException>(() => _testItem.UpdateFullName(lastName, firstName, middleName));
+        }
+
+        [TestCase(null, null, null)]
+        [TestCase("Test", null, null)]
+        [TestCase("Test", "Test", null)]
+        [TestCase(null, "Test", "Test")]
+        [TestCase(null, null, "Test")]
+        [TestCase("Test", null, "Test")]
+        public void ThrowsArgumentNullExceptionGivenEmptyName(string lastName, string firstName, string middleName)
+        {
+            Assert.Throws<ArgumentNullException>(() => _testItem.UpdateFullName(lastName, firstName, middleName));
+        }
+    }
+}
---
+++ tests/UnitTests/ApplicationCore/Entities/PersonItemTests/UpdateNameCard.cs
@@ -0,0 +1,46 @@
+﻿using Metcom.CardPay3.ApplicationCore.Entities.PersonAggregate;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metcom.CardPay3.UnitTests.ApplicationCore.Entities.PersonItemTests
+{
+    public class UpdateNameCard
+    {
+        private PersonItem _testItem;
+
+        private int _validId = 1;
+        private string _validLastName = "TestLastName";
+        private string _validFirstName = "TestFirstName";
+        private string _validMiddleName = "TestMiddleName";
+        private string _validLatinFirstName = "TestFirstLatinName";
+        private string _validLatinLastName = "TestLastLatinName";
+        private int _validGenderId = 1;
+        private int _validDocumentId = 1;
+
+        [SetUp]
+        public void UpdateNameCardSetUp()
+        {
+            _testItem = new PersonItem(_validId,_validLastName,_validFirstName, _validMiddleName, _validLatinFirstName, _validLatinLastName, _validGenderId, _validDocumentId);
+        }
+
+        [TestCase("", "")]
+        [TestCase("Test", "")]
+        [TestCase("", "Test")]
+        public void ThrowsArgumentExceptionGivenEmptyName(string lastName, string firstName)
+        {
+            Assert.Throws<ArgumentException>(() => _testItem.UpdateNameCard(lastName, firstName));
+        }
+
+        [TestCase(null, null)]
+        [TestCase("Test", null)]
+        [TestCase(null, "Test")]
+        public void ThrowsArgumentNullExceptionGivenEmptyName(string lastName, string firstName)
+        {
+            Assert.Throws<ArgumentNullException>(() => _testItem.UpdateNameCard(lastName, firstName));
+        }
+    }
+}
