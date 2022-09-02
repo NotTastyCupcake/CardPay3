33432556abbb19344c531dfdc13615ed1c4c3a3c
NotTastyCupcake <andrei28122000@gmail.com>
2022-09-02 15:28:14 +0500

    test: Тест удаления удалени персоны

---
+++ tests/UnitTests/ApplicationCore/Service/AccrualServiceTests/DeleteItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Metcom.CardPay3.ApplicationCore.Specifications;
+using Metcom.CardPay3.ApplicationCore.Services;
+
+namespace Metcom.CardPay3.UnitTests.ApplicationCore.Service.AccrualServiceTests
+{
+	public class DeleteItem
+	{
+		
+	}
+}
