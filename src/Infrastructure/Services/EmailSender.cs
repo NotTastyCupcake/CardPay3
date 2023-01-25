a629eec421186faa78e98da6d72fb4ef4bdbb877
Губин Андрей Дмитриевич <andrei28122000@gmail.com>
2023-01-25 17:08:01 +0500

    INT-28894 feat Добавлен класс отправки сообещний по почте

---
+++ src/Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,17 @@
+﻿using Metcom.CardPay3.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metcom.CardPay3.Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
