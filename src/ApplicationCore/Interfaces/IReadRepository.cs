7b2ac06702cb74c858865629c42183808d04f67f
Губин Андрей Дмитриевич <GubinAD@metcom.ru>
2023-04-12 17:15:40 +0500

    INT-28894 feat Добавления сервиса
    Доработал сервис репозиторий.

---
+++ src/ApplicationCore/Interfaces/IReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metcom.CardPay3.ApplicationCore.Interfaces
+{
+    public interface IReadRepository<T> : IReadRepositoryBase<T> where T : class 
+    {
+        
+    }
+}
