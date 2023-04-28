a4f0d68e145260c11b99ed68c1cf36a790a0af44
Губин Андрей Дмитриевич <GubinAD@metcom.ru>
2023-04-28 16:11:48 +0500

    INT-28894 feat Добавил форму для управления организациями.

---
+++ src/WebApplication/Controllers/OrganizationController.cs
@@ -0,0 +1,30 @@
+﻿using Metcom.CardPay3.ApplicationCore.Entities;
+using Metcom.CardPay3.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metcom.CardPay3.WebApplication.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class OrganizationController : Controller
+    {
+        private readonly IRepository<Organization> _repository;
+
+        public OrganizationController(IRepository<Organization> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet()]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
