caa41243a7e68f276e2281c9f662bbb51a41e59b
Губин Андрей Дмитриевич <GubinAD@metcom.ru>
2023-07-18 17:46:04 +0500

    INT-28894 feat Разработка формы, добавление ComboBox с выгрузкой коллекции из ViewModel

--- src/UI/WpfApplication/Views/MainWindow.xaml.cs
+++ src/UI/WpfApplication/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Metcom.CardPay3.WpfApplication.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ReactiveUI;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace Metcom.CardPay3.WpfApplication
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<HomeViewModel>
     {
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty
+            .Register(nameof(ViewModel), typeof(HomeViewModel), typeof(MainWindow));
+
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new HomeViewModel();
+            //TODO: Добавить значение "Создать организацию"
+            this.WhenActivated(disposable => { 
+                this.OneWayBind(this.ViewModel, 
+                                vm => vm.Organizations,
+                                view => view.Organizations.ItemsSource)
+                .DisposeWith(disposable); });
         }
+
+        public HomeViewModel ViewModel { get => (HomeViewModel)GetValue(ViewModelProperty); set => SetValue(ViewModelProperty, value); }
+        object IViewFor.ViewModel { get => ViewModel; set => ViewModel = (HomeViewModel)value; }
     }
 }
